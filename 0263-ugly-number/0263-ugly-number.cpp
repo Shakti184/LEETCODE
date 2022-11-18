8cce163506c22d8c0ce0e52b79b08f65478034cb
Shakti Maddheshiya <97889623+Shakti184@users.noreply.github.com>
2022-11-18 13:53:57 +0530

    Time: 0 ms (100.00%), Space: 6 MB (19.97%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -2,18 +2,26 @@ class Solution {
 public:
     bool isUgly(int n) {
         if(n==0)  return false;
-        while(n%2==0){
-            n/=2;
+        int arr[3]={2,3,5};
+        for(int i=0;i<3;i++){
+            while(n%arr[i]==0){
+                n/=arr[i];
+            }
         }
-        while(n%3==0){
-            n/=3;
-        }
-        while(n%5==0){
-            n/=5;
-        }
-        if(n==1){
-            return true;
-        }
-        return false;
+        return n==1;
+        
+        // while(n%2==0){
+        //     n/=2;
+        // }
+        // while(n%3==0){
+        //     n/=3;
+        // }
+        // while(n%5==0){
+        //     n/=5;
+        // }
+        // if(n==1){
+        //     return true;
+        // }
+        // return false;
     }
 };
