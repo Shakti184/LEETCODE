8b6477a884be667aa4c6f475137d14130f512011
Shakti Maddheshiya <97889623+Shakti184@users.noreply.github.com>
2023-01-25 01:56:30 +0530

    Added solution - LeetHub

---
+++ Good Numbers - GFG/good-numbers.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    vector<int> goodNumbers(int L, int R, int D) {
+        // code here
+        vector<int>v;
+        for(int i=L;i<=R;i++){
+            int x=i;
+            if(i==D)continue;
+            int c=0;
+            int m=INT_MIN;
+            while(x){
+                int a=x%10;
+                x/=10;
+                if(a==D){
+                    c=1;
+                    break;
+                }
+                if(a<=m){
+                    c=1;
+                }
+                if(m==INT_MIN)m=a;
+                else m+=a;
+            }
+            
+            if(c==0){
+                if(i!=D)
+                v.push_back(i);
+            }
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int L, R, D;
+        cin >> L >> R >> D;
+        Solution ob;
+        vector<int> ans = ob.goodNumbers(L, R, D);
+        for (auto u : ans) cout << u << " ";
+        cout << "\n";
+    }
+}
+// } Driver Code Ends
