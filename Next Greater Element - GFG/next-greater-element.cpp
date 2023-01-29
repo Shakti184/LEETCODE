931e7b34a28e8eb38643b069679bfe70a7eb5965
Shakti Maddheshiya <97889623+Shakti184@users.noreply.github.com>
2023-01-29 19:49:04 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.cpp
+++ Next Greater Element - GFG/next-greater-element.cpp
@@ -11,25 +11,29 @@ class Solution
     vector<long long> nextLargerElement(vector<long long> arr, int n){
         // Your code here
         stack<long long>st;
-        vector<long long >ans(n);
+        
         for(int i=n-1;i>=0;i--){
             if(i==n-1){
-                ans[i]=-1;
                 st.push(arr[i]);
+                arr[i]=-1;
             }
             else{
                 while(!st.empty()&&arr[i]>=st.top()){
                     st.pop();
                 }
                 
-                if(st.empty())ans[i]=-1;
-                else {
-                    ans[i]=st.top();
+                if(st.empty()){
+                    st.push(arr[i]);
+                    arr[i]=-1;
                 }
-                st.push(arr[i]);
+                else {
+                    long long int x=st.top();
+                    st.push(arr[i]);
+                    arr[i]=x;
+                }
             }
         }
-        return ans;
+        return arr;
     }
 };
 
