6e4c6117394de8654846d67932e65105b03645f8
Shakti Maddheshiya <97889623+Shakti184@users.noreply.github.com>
2023-10-24 23:41:02 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Palindromic Partitioning/palindromic-partitioning.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+// Initial Template for c++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int palindromicPartition(string s)
+    {
+        // code here
+        int n=s.length();
+        vector<vector<bool>>isPalindrome(n+1,vector<bool>(n+1,0));
+        for(int i=0;i<n;i++)
+        {
+            isPalindrome[i][i]=1;
+        }
+        for(int gap=1;gap<n;gap++)
+        {
+            for(int i=0;i<n-gap;i++)
+            {
+                int j=i+gap;
+                if(gap==1)
+                {
+                    if(s[i]==s[j])
+                    isPalindrome[i][j]=1;
+                }
+                else
+                {
+                    if((isPalindrome[i+1][j-1]==1) && s[i]==s[j])
+                    isPalindrome[i][j]=1;
+                }
+            }
+        }
+        // cout<<"HI"<<endl;
+        // for(auto it:isPalindrome)
+        // {
+        //     for(auto i:it)
+        //     cout<<i<<" ";
+        //     cout<<endl;
+        // }
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        function<int(int,int)>f=[&](int i,int prev)->int{
+           if(i==n-1)
+           {
+               if(isPalindrome[prev][i])
+               return 0;
+               else
+               return 0x3f3f3f3f;
+           }
+           if(dp[i][prev]!=-1)
+           return dp[i][prev];
+           int x1=f(i+1,prev);
+           int x2=INT_MAX;
+           if(isPalindrome[prev][i])
+           {
+               x2=1+f(i+1,i+1);
+           }
+           int res=min(x1,x2);
+           return dp[i][prev]=res;
+        };
+        return f(0,0);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout<<ob.palindromicPartition(str)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
